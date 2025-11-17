15e739ef35b287edf9e82850fdb7ed3b85ad41ff
Keshav Haripaul <keshav.haripaul@yahoo.com>
2025-11-17 17:26:54 +0200

    Added hr controller showcasing code used to build hr interface

---
+++ prog6212-part3-ST10456157/Controllers/HrController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prog6212_part3_ST10456157.Data;
+
+namespace prog6212_part3_ST10456157.Controllers
+{
+    public class HrController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HrController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult ApprovedClaims()
+        {
+            return View(_context.Claims.Where(c => c.Status == "Approved").ToList());
+        }
+    }
+
+}
